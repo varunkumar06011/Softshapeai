b68dc397d8220bfacdc8ec96f51e02e476e36008
Akhil14324 <akhilvallabhani@gmail.com>
2026-06-05 11:53:56 +0530

    egreess fix

--- src/config/captains.js
+++ src/config/captains.js
@@ -4,5 +4,5 @@ export const CAPTAINS = [
   { id: 'C3', name: 'Sagar', pin: '2000', initials: 'S', color: 'bg-[#ECFDF5] text-[#047857]' },
   { id: 'C4', name: 'Durga Prasad', pin: '1998', initials: 'DP', color: 'bg-[#FFF1F2] text-[#BE123C]' },
   { id: 'C5', name: 'Subbaiah', pin: '1977', initials: 'SU', color: 'bg-[#FEF3C7] text-[#D97706]' },
-  { id: 'C6', name: 'Happy', pin: '1996', initials: 'H', color: 'bg-[#F3E8FF] text-[#7E22CE]' },
+  { id: 'C6', name: 'Akhil', pin: '2000', initials: 'A', color: 'bg-[#F3E8FF] text-[#7E22CE]' },
 ];
